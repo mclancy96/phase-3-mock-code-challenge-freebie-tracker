c1cd475ef700774f9e530edf4ec4286879bdbf9f
Mike <mclancy96@gmail.com>
2025-07-28 13:55:29 -0400

    Dev methods

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,4 +1,16 @@
+require 'pry'
 class Dev < ActiveRecord::Base
   has_many :freebies
   has_many :companies, through: :freebies
+
+  def received_one?(item_name)
+    freebies.map(&:item_name).include?(item_name)
+  end
+
+  def give_away(dev, freebie)
+    return unless freebie.dev_id == id
+
+    freebie.dev_id = dev.id
+    freebie.save
+  end
 end
