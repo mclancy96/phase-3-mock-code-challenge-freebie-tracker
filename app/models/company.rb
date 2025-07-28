d54f1d321ab32c8f30fa7622a0dea97514ceb464
Mike <mclancy96@gmail.com>
2025-07-28 13:40:33 -0400

    Company methods

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,12 @@
 class Company < ActiveRecord::Base
   has_many :freebies
   has_many :devs, through: :freebies
+
+  def give_freebie(dev, item_name, value)
+    Freebie.create(dev: dev, company: self, item_name: item_name, value: value)
+  end
+
+  def self.oldest_company
+    Company.all.order(founding_year: :asc).first
+  end
 end
